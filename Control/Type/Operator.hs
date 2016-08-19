0fc211ee729a679740bd1631266d3a0f836c72df
Shou <x@shou.io>
2016-08-19 22:42:14 +0100

    Cfor and Cmap to operators

--- Control/Type/Operator.hs
+++ Control/Type/Operator.hs
@@ -1,83 +1,100 @@
 
 {-# LANGUAGE TypeOperators, LiberalTypeSynonyms, RankNTypes,
              ConstraintKinds, KindSignatures, TypeFamilies,
-             ImpredicativeTypes, PolyKinds, DataKinds #-}
+             PolyKinds, DataKinds, CPP, MultiParamTypeClasses,
+             UndecidableSuperClasses, FlexibleInstances #-}
 
 -- | A collection of type-level operators.
-module Control.Type.Operator where
+module Control.Type.Operator ( type (^>), type (<^), type ($), type (&)
+                             , type ($$), type (<+>), type (<=>)
+                             ) where
 
 
-import Data.Kind
+#if __GLASGOW_HASKELL__ <= 710
+import GHC.Prim (Constraint)
+#else
+import Data.Kind (Constraint)
+#endif
+
+
+-- | Empty typeclass constraint.
+class Empty a
+instance Empty a
 
 
 -- | A tightly binding version of @->@ that lets you strip parentheses from
 -- function types in certain spots. Example:
 --
--- >>> f :: Maybe Int ^> String
+-- @
+-- f :: Maybe Int ^> String
+-- =
 -- f :: Maybe (Int -> Int)
+-- @
 type (^>) = (->)
 infixr 5 ^>
 
 -- | A flipped '^>'.
 --
--- >>> f :: Maybe String <^ Int
+-- @
+-- f :: Maybe String <^ Int
+-- @
 --
 -- Note: this is not partially applied like '^>' and @->@.
 type (<^) a b = (^>) b a
+infixr 5 <^
 
 -- | Infix application.
 --
--- >>> f :: Either String $ Maybe Int
+-- @
+-- f :: Either String $ Maybe Int
+-- =
 -- f :: Either String (Maybe Int)
+-- @
 type f $ a = f a
 infixr 2 $
 
 -- | A flipped '$'.
 --
--- >>> f :: Maybe Int & Maybe
+-- @
+-- f :: Maybe Int & Maybe
+-- =
 -- f :: Maybe (Maybe Int)
+-- @
 type a & f = f a
 infixl 1 &
 
 -- | Infix application that takes a two arguments rather than just one.
 --
--- >>> f :: Either $$ Int ^> Int $ Int
+-- @
+-- f :: Either $$ Int ^> Int $ Int
+-- =
 -- f :: Either (Int -> Int) Int
+-- @
 type (f $$ a) b = f a b
 infixr 3 $$
 
 -- | Map a constraint over several variables.
 --
--- >>> a :: Cmap Show [a, b] => a -> b -> String
--- >>> =
--- >>> a :: (Show a, Show b) => a -> b -> String
-type family Cmap (c :: k -> Constraint) (ls :: [k]) where
-    Cmap c '[k] = c k
-    Cmap c (h ': t) = (c h, Cmap c t)
-
--- | Infix 'Cmap'.
---
--- >>> a :: Show <=> [a, b] => a -> b -> String
--- >>> =
--- >>> a :: (Show a, Show b) => a -> b -> String
-type (c <=> ls) = Cmap c ls
+-- @
+-- a :: Show \<=> [a, b] => a -> b -> String
+-- =
+-- a :: (Show a, Show b) => a -> b -> String
+-- @
+type family (<=>) (c :: k -> Constraint) (as :: [k]) where
+    (<=>) c '[k] = c k
+    (<=>) c (h ': t) = (c h, (<=>) c t)
 infixl 9 <=>
 
 -- | Map several constraints over a single variable.
 --
--- >>> a :: Cfor [Show, Read] a => a -> a
--- >>> =
--- >>> a :: (Num a, Show a, Read a) => a -> IO ()
-type family Cfor (c :: [k -> Constraint]) (a :: k) where
-    Cfor '[c] a = c a
-    Cfor (ch ': ct) a = (ch a, Cfor ct a)
-
--- | Infix 'Cfor'.
---
--- >>> a :: [Show, Read] <+> a => a -> a
--- >>> =
--- >>> a :: (Num a, Show a, Read a) => a -> IO ()
-type (cs <+> a) = Cfor cs a
+-- @
+-- a :: [Show, Read] \<+> a => a -> a
+-- =
+-- a :: (Show a, Read a) => a -> a
+-- @
+type family (<+>) (c :: [k -> Constraint]) (a :: k) where
+    (<+>) '[] a = Empty a
+    (<+>) '[c] a = c a
+    (<+>) (ch ': ct) a = (ch a, (<+>) ct a)
 infixl 9 <+>
-
 
